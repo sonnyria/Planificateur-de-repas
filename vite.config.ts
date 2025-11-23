9aa3ee147ee55a96919397d84963a181b532708a
sonnyria <sonnyria@gmail.com>
2025-11-23 16:07:06 +0100

    Fix vite.config.ts syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-      base: '/Planificateur-de-repas/',
     return {
+            base: '/Planificateur-de-repas/',
       server: {
         port: 3000,
         host: '0.0.0.0',
